f77a1b6a0b0039cd0599c15b4890b2d86fcb73fa
Aurelio Killian Lexi Verrill <87407047+aurelioklv@users.noreply.github.com>
2023-10-12 15:14:57 +0000

    feat: add custom error page and apache2 config for abimanyu.f12.com

--- scripts/f12_abimanyu.sh
+++ scripts/f12_abimanyu.sh
@@ -78,11 +78,122 @@ echo -e "${BG_GREEN}Success configuring arjuna.f12 in /etc/nginx/sites-available
 
 
 # Configure DocumentRoot to /var/www/abimanyu.f12
-config_file="/etc/apache2/sites-available/000-default.conf"  # Change this to the actual path of your Apache configuration file
-new_document_root="/var/www/abimanyu.f12"
-echo -e "${BG_BLUE}Configuring DocumentRoot to $new_document_root ...${RESET}"
-sed -i "s|DocumentRoot .*$|DocumentRoot $new_document_root|" "$config_file"
-echo -e "${BG_GREEN}DocumentRoot configuration success ...${RESET}"
+echo -e "${BG_BLUE} Configuring /etc/apache2/sites-available/000-default.conf ...${RESET}"
+cat <<EOF > /etc/apache2/sites-available/000-default.conf
+<VirtualHost *:80>
+        # The ServerName directive sets the request scheme, hostname and port that
+        # the server uses to identify itself. This is used when creating
+        # redirection URLs. In the context of virtual hosts, the ServerName
+        # specifies what hostname must appear in the request's Host: header to
+        # match this virtual host. For the default virtual host (this file) this
+        # value is not decisive as it is used as a last resort host regardless.
+        # However, you must set it for any further virtual host explicitly.
+        #ServerName www.example.com
+
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/abimanyu.f12
+
+        # Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
+        # error, crit, alert, emerg.
+        # It is also possible to configure the loglevel for particular
+        # modules, e.g.
+        #LogLevel info ssl:warn
+
+        ErrorLog \${APACHE_LOG_DIR}/error.log
+        CustomLog \${APACHE_LOG_DIR}/access.log combined
+        ErrorDocument 404 /c403.html
+        ErrorDocument 404 /c404.html
+        # For most configuration files from conf-available/, which are
+        # enabled or disabled at a global level, it is possible to
+        # include a line for only one particular virtual host. For example the
+        # following line enables the CGI configuration for this host only
+        # after it has been globally disabled with "a2disconf".
+        #Include conf-available/serve-cgi-bin.conf
+</VirtualHost>
+
+# vim: syntax=apache ts=4 sw=4 sts=4 sr noet
+EOF
+echo -e "${BG_GREEN} Successful configuring /etc/apache2/sites-available/000-default.conf ...${RESET}"
+
+
+# Customizing error page
+echo -e "${BG_BLUE} Customizing error page ...${RESET}"
+touch /var/www/abimanyu.f12/c403.html /var/www/abimanyu.f12/c404.html
+cat <<EOF > /var/www/abimanyu.f12/c403.html
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Error 403 - Forbidden</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+        }
+        .container {
+            margin-top: 50px;
+        }
+        h1 {
+            font-size: 72px;
+            color: #FF0000;
+        }
+        p {
+            font-size: 24px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Error 403, dumbass</h1>
+        <p>Forbidden to enter, need permission. you criminal</p>
+    </div>
+</body>
+</html>
+EOF
+echo -e "${BG_GREEN}c403.html created successfully${RESET}"
+cat <<EOF > /var/www/abimanyu.f12/c404.html
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Error 404 - Page Not Found</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+        }
+        .container {
+            margin-top: 50px;
+        }
+        h1 {
+            font-size: 72px;
+            color: #FF0000;
+        }
+        p {
+            font-size: 24px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Error 404, theres NOTHING</h1>
+        <p>Theres nothing here, youre either an idiot or mentally ill</p>
+    </div>
+</body>
+</html>
+EOF
+echo -e "${BG_GREEN}c404.html created successfully${RESET}"
+
+
+# Configure .htaccess
+echo -e "${BG_BLUE}Configuring .htaccess on /var/www/abimanyu.f12 ...${RESET}"
+touch /var/www/abimanyu.f12/.htaccess
+cat <<EOF > /var/www/abimanyu.f12/.htaccess
+RewriteEngine On
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteRule ^([^\.]+)$ $1.php [NC,L]
+EOF
+echo -e "${BG_BLUE}Configuring .htaccess on /var/www/abimanyu.f12 ...${RESET}"
 
 # Download resources using wget
 echo -e "${BG_BLUE}Downloading resources...${RESET}"
