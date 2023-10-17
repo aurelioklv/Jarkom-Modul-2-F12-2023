5dd495dcd555ae1ae9c6ae9e8f131f825361ed19
RanoCg123 <114126015+RanoCg123@users.noreply.github.com>
2023-10-17 19:44:36 +0700

    Update f12_abimanyu.sh

--- scripts/f12_abimanyu.sh
+++ scripts/f12_abimanyu.sh
@@ -137,6 +137,9 @@ cat <<EOF > /etc/apache2/sites-available/parikesit-default.conf
                 Require all denied
         </Directory>
         Alias /js /var/www/parikesit.abimanyu.f12/public/js
+        RewriteEngine On
+        RewriteCond %{REQUEST_URI} abimanyu
+        RewriteRule ^(.*)$ /var/www/parikesit.abimanyu.f12/public/images/abimanyu.png
         # Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
         # error, crit, alert, emerg.
         # It is also possible to configure the loglevel for particular
@@ -145,6 +148,8 @@ cat <<EOF > /etc/apache2/sites-available/parikesit-default.conf
 
         ErrorLog \${APACHE_LOG_DIR}/error.log
         CustomLog \${APACHE_LOG_DIR}/access.log combined
+        ErrorDocument 403 /error/403.html
+        ErrorDocument 404 /error/404.html
         # For most configuration files from conf-available/, which are
         # enabled or disabled at a global level, it is possible to
         # include a line for only one particular virtual host. For example the
@@ -227,7 +232,7 @@ unzip /var/www/rjp.baratayuda.abimanyu.f12.com.zip -d /var/www/
 mv /var/www/rjp.baratayuda.abimanyu.yyy.com /var/www/rjp.baratayuda.abimanyu.f12
 rm /var/www/rjp.baratayuda.abimanyu.f12.com.zip
 echo -e "${BG_GREEN}Unpacking successful...${RESET}"
-
+a2enmod rewrite
 a2ensite parikesit-default.conf
 a2ensite rjp-default.conf
 
